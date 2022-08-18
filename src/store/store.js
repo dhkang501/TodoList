3acfddfc54561edf2c0ca35abe3169f12a786b6f
dhkang501 <46888650+dhkang501@users.noreply.github.com>
2022-08-18 14:52:23 +0900

    store.js 수정

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import todoApp from './modules/todoApp'
-// import * as getters from './getters'
-// import * as mutations from './mutations'
 
 Vue.use(Vuex);
 
